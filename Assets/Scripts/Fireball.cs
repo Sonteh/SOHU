0d3457e51433c425c9c9ba0bf5b7b52663fe47af
Gracjan Redwanc <redwanc.gracjan@gmail.com>
2021-01-31 16:24:56 +0100

    Prototype of a game with isometric view, working mirror and 1 spell(need fixes!)

---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 7.0f;
+    [SerializeField]
+    private float _outOfRange = 2.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Vector3 mouse = Input.mousePosition;
+            Ray ray = Camera.main.ScreenPointToRay(mouse);
+            RaycastHit hit;
+            Physics.Raycast(ray, out hit);
+
+            Vector3 target = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+
+            
+        }
+
+        //transform.position = Vector3.MoveTowards(transform.position, ray.origin, _speed * Time.deltaTime);
+
+
+        Ray ray2 = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit2;
+        Physics.Raycast(ray2, out hit2);
+     
+        transform.Translate(ray2.origin * _speed * Time.deltaTime);
+   
+
+
+        Destroy(this.gameObject, _outOfRange);
+  
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _fireballPrefab;
+    [SerializeField]
+    private float _fireballCooldown = 0.5f;
+    private float _canUseFireball = -1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q) && Time.time > _canUseFireball)
+        {
+            UseFireball();
+        }
+    }
+
+    private void UseFireball()
+    {
+        _canUseFireball = _fireballCooldown + Time.time;
+        Instantiate(_fireballPrefab, transform.position + Vector3.forward, Quaternion.identity);
+
+        
+    }
+}
