081766d1cc86d023cdf518b0968645ccfce33442
Wojciech Kudlacik <kudlacik.wojciech@gmail.com>
2021-03-13 18:03:53 +0100

    changes

---
+++ Assets/Scripts/Menus/StartMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+    [SerializeField] private Network networkManager = null;
+
+    [Header("Menu")]
+    [SerializeField] private GameObject main = null;
+    [SerializeField] private GameObject startMenu = null;
+
+    void Start()
+    {
+        Button buttonBack = transform.Find("ButtonBack").GetComponent<Button>();
+
+        buttonBack.onClick.AddListener(GoBack);
+
+    }
+
+    void GoBack()
+    {
+        main.SetActive(true);
+        startMenu.SetActive(false);
+    }
+
+    public void HostLobby()
+    {
+        networkManager.StartHost();
+
+        startMenu.SetActive(false);
+
+    }
+
+}
