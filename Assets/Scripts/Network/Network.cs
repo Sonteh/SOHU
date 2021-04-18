a233ad58d8f52fe68d8dd082eeaa82d25243350c
Gracjan Redwanc <redwanc.gracjan@gmail.com>
2021-04-18 15:39:19 +0200

    Begining of round system, animation of starting round, cleaning folders and files

--- Assets/Scripts/Network/Network.cs
+++ Assets/Scripts/Network/Network.cs
@@ -19,19 +19,19 @@ public class Network : NetworkManager
     [Header("Game")]
     [SerializeField] private NetworkPlayer gamePlayerPrefab = null;
     [SerializeField] private GameObject playerSpawnSystem = null;
+    [SerializeField] private GameObject roundSystem = null;
 
     // Network Events
     public static event Action ClientOnConnected;
     public static event Action ClientOnDisconnected;
     public static event Action<NetworkConnection> OnServerReadied;
+    public static event Action OnServerStopped;
 
     // List of Players in the Lobby
     public List<NetworkRoom> RoomPlayers { get; } = new List<NetworkRoom>();
 
     // List of Players in the Game
     public List<NetworkPlayer> GamePlayers { get; } = new List<NetworkPlayer>();
-
-
 
     public override void OnStartServer() => spawnPrefabs = Resources.LoadAll<GameObject>("SpawnablePrefabs").ToList();
 
@@ -113,7 +113,10 @@ public class Network : NetworkManager
 
     public override void OnStopServer()
     {
+        OnServerStopped?.Invoke();
+
         RoomPlayers.Clear();
+        GamePlayers.Clear();
     }
 
     public void NotifyPlayersOfReadyState()
@@ -124,7 +127,6 @@ public class Network : NetworkManager
         }
 
     }
-
 
     private bool IsReadyToStart()
     {
@@ -178,6 +180,9 @@ public class Network : NetworkManager
         {
             GameObject playerSpawnSystemInstance = Instantiate(playerSpawnSystem);
             NetworkServer.Spawn(playerSpawnSystemInstance);
+
+            GameObject roundSystemInstance = Instantiate(roundSystem);
+            NetworkServer.Spawn(roundSystemInstance);
         }
     }
 
