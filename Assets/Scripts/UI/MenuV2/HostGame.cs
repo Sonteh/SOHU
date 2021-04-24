f90a64da9d1dabd862621db2d48f0ce38809de71
Gracjan Redwanc <redwanc.gracjan@gmail.com>
2021-04-24 13:38:59 +0200

    Stashing

---
+++ Assets/Scripts/UI/MenuV2/HostGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class HostGame : MonoBehaviour
+{
+    public void HostLobby()
+    {
+        NetworkManager.singleton.StartHost();
+    }
+
+/* TODO
+    public void Refresh()
+    {
+        Debug.Log("here");
+
+        var pd = APIHelper.GetPlayers();
+
+        Debug.Log(pd.data[0].IP);
+    }
+    */
+}
