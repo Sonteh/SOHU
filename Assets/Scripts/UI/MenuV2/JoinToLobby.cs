aa8104aee22234feeafd99d37b45b874066fccb4
Gracjan Redwanc <redwanc.gracjan@gmail.com>
2021-04-25 18:38:49 +0200

    Joining lobby by inserting IP Adress functionality added

---
+++ Assets/Scripts/UI/MenuV2/JoinToLobby.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class JoinToLobby : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] private TMP_InputField ipAddressInputField;
+    [SerializeField] private Button joinButton;
+    [SerializeField] private Button closeButton;
+
+    private void OnEnable()
+    {
+        Network.ClientOnConnected += HandleClientConnected;
+        Network.ClientOnDisconnected += HandleClientDisconnected;
+    }
+
+    private void OnDisable()
+    {
+        Network.ClientOnConnected -= HandleClientConnected;
+        Network.ClientOnDisconnected -= HandleClientDisconnected;
+    }
+
+    public void JoinLobby()
+    {
+        string ipAddress = ipAddressInputField.text;
+
+        NetworkManager.singleton.networkAddress = ipAddress;
+        NetworkManager.singleton.StartClient();
+
+        joinButton.interactable = false;
+
+    }
+
+    public void CloseIpDialog()
+    {
+        Debug.Log("here");
+        //gameObject.SetActive(false);
+        //startMenu.SetActive(true);
+        Debug.Log("here after");
+    }
+
+    private void HandleClientConnected()
+    {
+        joinButton.interactable = true;
+
+       // gameObject.SetActive(false);
+       // startMenu.SetActive(false);
+    }
+
+    private void HandleClientDisconnected()
+    {
+        joinButton.interactable = true;
+    }
+}
