b3e6dcc986df84345e14f6b011351f01c7a563ee
Omik Futerkowski <dawidszab@wp.pl>
2021-05-26 00:55:13 +0200

    Basic player animations

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -4,6 +4,28 @@ using Mirror;
 
 public class Mover : NetworkBehaviour
 {
+
+    private Animator animator;
+    public Vector3 lastPos;
+    public Vector3 curPos;
+
+    public Rigidbody rb;
+    CharacterController controller;
+    NavMeshAgent test;
+
+    private void Awake() {
+        animator = GetComponentInChildren<Animator> ();
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //controller = GetComponent<CharacterController>();
+        // = GetComponent<CharacterController>();
+        test = GetComponent<NavMeshAgent>();
+    }
+
+    [ClientCallback]
     private void Update()
     {
         if (!isLocalPlayer) {return;}
@@ -11,7 +33,68 @@ public class Mover : NetworkBehaviour
         if (Input.GetButton("Fire2"))
         {
             MoveToCursor();
+            //animator.SetFloat("movement", 1);
         }
+
+        // if (!hasAuthority) {return;}
+        // MotionChecker();
+        if(test.velocity != Vector3.zero) 
+     {
+         animator.SetFloat("movement", 1);
+     }
+     else
+     {
+         animator.SetFloat("movement", 0);
+     }
+    }
+
+    //[Client]
+    [Command]
+    private void MotionChecker()
+    {
+        // Debug.Log("MYSZ" + GetPlayerMousePosition().point);
+        // Debug.Log("GRACZ" + transform.position);
+        // // lastPos = transform.position;
+        // // yield return new WaitForSeconds(1);
+        // if (GetPlayerMousePosition().point == transform.position)
+        // {
+        //     animator.SetFloat("movement", 1);
+        // }
+        // else
+        // {
+        //     animator.SetFloat("movement", 0);
+        // }
+        ChangeParameter();
+    }
+
+    [ClientRpc]
+    private void ChangeParameter()
+    {
+    // curPos = transform.position;
+    //  if(curPos == lastPos) 
+    //  {
+    //      animator.SetFloat("movement", 1);
+    //  }
+    //  else
+    //  {
+    //      animator.SetFloat("movement", 0);
+    //  }
+    //  lastPos = curPos;
+    
+    //float overallSpeed = controller.velocity.magnitude;
+    // float velocity = agent.velocity.magnitude/agent.speed;
+    // float speed = rb.velocity;
+
+    //GOOOOOOOOOOOD
+    Debug.Log("velocity: " + test.velocity);
+    if(test.velocity != Vector3.zero) 
+     {
+         animator.SetFloat("movement", 1);
+     }
+     else
+     {
+         animator.SetFloat("movement", 0);
+     }
     }
 
     private void MoveToCursor()
