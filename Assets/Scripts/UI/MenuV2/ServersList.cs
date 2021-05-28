464068d192ac88a90c07372831f30b115498080c
Wojciech Kudlacik <kudlacik.wojciech@gmail.com>
2021-05-28 12:55:37 +0200

    refresh button + list view

---
+++ Assets/Scripts/UI/MenuV2/ServersList.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServersList : MonoBehaviour
+{
+    public void Refresh()
+    {
+        Debug.Log("here");
+
+        var pd = APIHelper.GetPlayers();
+
+        Debug.Log(pd.data[0].IP);
+    }
+}
