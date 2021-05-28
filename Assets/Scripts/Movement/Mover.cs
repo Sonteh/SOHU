1b98309db60763cea9a981f7d76929681aa03f8d
Omik Futerkowski <dawidszab@wp.pl>
2021-05-28 21:22:03 +0200

    Cleanup

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -20,8 +20,6 @@ public class Mover : NetworkBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        //controller = GetComponent<CharacterController>();
-        // = GetComponent<CharacterController>();
         test = GetComponent<NavMeshAgent>();
     }
 
@@ -33,68 +31,16 @@ public class Mover : NetworkBehaviour
         if (Input.GetButton("Fire2"))
         {
             MoveToCursor();
-            //animator.SetFloat("movement", 1);
         }
 
-        // if (!hasAuthority) {return;}
-        // MotionChecker();
         if(test.velocity != Vector3.zero) 
-     {
-         animator.SetFloat("movement", 1);
-     }
-     else
-     {
-         animator.SetFloat("movement", 0);
-     }
-    }
-
-    //[Client]
-    [Command]
-    private void MotionChecker()
-    {
-        // Debug.Log("MYSZ" + GetPlayerMousePosition().point);
-        // Debug.Log("GRACZ" + transform.position);
-        // // lastPos = transform.position;
-        // // yield return new WaitForSeconds(1);
-        // if (GetPlayerMousePosition().point == transform.position)
-        // {
-        //     animator.SetFloat("movement", 1);
-        // }
-        // else
-        // {
-        //     animator.SetFloat("movement", 0);
-        // }
-        ChangeParameter();
-    }
-
-    [ClientRpc]
-    private void ChangeParameter()
-    {
-    // curPos = transform.position;
-    //  if(curPos == lastPos) 
-    //  {
-    //      animator.SetFloat("movement", 1);
-    //  }
-    //  else
-    //  {
-    //      animator.SetFloat("movement", 0);
-    //  }
-    //  lastPos = curPos;
-    
-    //float overallSpeed = controller.velocity.magnitude;
-    // float velocity = agent.velocity.magnitude/agent.speed;
-    // float speed = rb.velocity;
-
-    //GOOOOOOOOOOOD
-    Debug.Log("velocity: " + test.velocity);
-    if(test.velocity != Vector3.zero) 
-     {
-         animator.SetFloat("movement", 1);
-     }
-     else
-     {
-         animator.SetFloat("movement", 0);
-     }
+        {
+            animator.SetFloat("movement", 1);
+        }
+        else
+        {
+            animator.SetFloat("movement", 0);
+        }
     }
 
     private void MoveToCursor()
