f66bc06dc30303a7403f620dcb29c32a896e3aec
Omik Futerkowski <dawidszab@wp.pl>
2021-06-13 17:33:11 +0200

    Pillars for Arena02

--- Assets/Scripts/Environment/EnvironmentDurability.cs
+++ Assets/Scripts/Environment/EnvironmentDurability.cs
@@ -6,12 +6,8 @@ public class EnvironmentDurability : NetworkBehaviour
 {
     [Header("Settings")]
     [SerializeField] public float maxHealth = 100f;
-    //[SerializeField] NetworkPlayer networkPlayer;
     private float damage;
-    private float heal;
-    [SerializeField] GameObject roundSystem;
     [SerializeField] GameObject destroyedModel;
-    //GameObject roundSystem; 
     [SyncVar]
     public float currentHealth;
     public delegate void HealthUpdateDelegate(float currentHealth, float maxHealth);
@@ -29,25 +25,12 @@ public class EnvironmentDurability : NetworkBehaviour
         SetHealth(maxHealth);
     }
 
-    //[Command]
     private void CmdDealDamage(float value)
     {
-        Debug.Log("Witam w deal damage");
         SetHealth(Mathf.Max(currentHealth - value, 0));
-        Debug.Log("co z tym max hp?: "+currentHealth);
         if (currentHealth == 0f)
         {
             RpcOnDeath();
-            //roundSystem.GetComponent<RoundSystem>().OnDeath(connectionToClient);
-        }
-    }
-
-    [Command]
-    public void CmdHealDamage(float value)
-    {
-        if (currentHealth <= 100f)
-        {
-            SetHealth(Mathf.Min(currentHealth + value, 100f));
         }
     }
 
@@ -60,36 +43,11 @@ public class EnvironmentDurability : NetworkBehaviour
     
     private void OnTriggerEnter(Collider collider)
     {
-        Debug.Log("sdddddddddddddddddddddddddddd");
-        
-        
         if (collider.tag == "Spell")
         {   
             damage = collider.gameObject.GetComponent<SpellData>().spellDamage;
-            Debug.Log("DEMEJDZ: "+ damage);
             CmdDealDamage(damage);
             Destroy(collider.gameObject);
-        }
-        if (collider.tag == "Heal")
-        {   
-            heal = collider.gameObject.GetComponent<SpellData>().healAmount;
-            CmdHealDamage(heal);
-        }
-    }
-
-    private void OnTriggerStay(Collider collider) 
-    {
-        if (!hasAuthority) { return; }
-        
-        if (collider.tag == "Zone")
-        {   
-            damage = collider.gameObject.GetComponent<ZoneData>().zoneDamage;
-            CmdDealDamage(damage);
-        }
-        if (collider.tag == "HealOverTime")
-        {   
-            heal = collider.gameObject.GetComponent<SpellData>().healAmount;
-            CmdHealDamage(heal);
         }
     }
 }
