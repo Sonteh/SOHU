9bd0ecde7462eab4cf1bda61abee181b693af9f7
Wojciech Kudlacik <kudlacik.wojciech@gmail.com>
2021-06-15 19:20:18 +0200

    join from list

--- Assets/Scripts/UI/MenuV2/HostGame.cs
+++ Assets/Scripts/UI/MenuV2/HostGame.cs
@@ -3,49 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 using System.Net;
+using System.Linq;
 
 public class HostGame : MonoBehaviour
 {
     public void HostLobby()
     {
         NetworkManager.singleton.StartHost();
-        IPAddress[] localIPs = Dns.GetHostAddresses("");
-        string localIp;
-        Debug.Log(localIPs[0]);
-        Debug.Log(localIPs[1]);
-        Debug.Log(localIPs[2]);
-        Debug.Log(localIPs[3]);
-        Debug.Log(localIPs[4]);
-        Debug.Log(localIPs[5]);
-        Debug.Log(localIPs[6]);
-        Debug.Log(localIPs[7]);
-        Debug.Log(localIPs[8]);
-        Debug.Log(localIPs[9]);
+        IPAddress[] localIPsFromDns = Dns.GetHostAddresses("");
+        IPAddress[] localIPsV4;
 
-        Debug.Log("len: " + localIPs.Length);
         if (Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor)
         {
-
+            localIPsV4 = localIPsFromDns.Reverse().Take(2).ToArray();
+            APIHelper.RegisterServer(FindIp(localIPsV4), PlayerPrefs.GetString("PlayerName"), "Arena01");
         }
         else if (Application.platform == RuntimePlatform.OSXEditor || Application.platform == RuntimePlatform.OSXPlayer)
         {
-            //IPAddress[] localIpsList = [local]
+            localIPsV4 = localIPsFromDns.Take(2).ToArray();
+            APIHelper.RegisterServer(FindIp(localIPsV4), PlayerPrefs.GetString("PlayerName"), "Arena01");
+
+        }
+    }
 
 
-            if (localIPs.Length == 2)
-            {
-                localIp = localIPs[1].ToString();
-                Debug.Log("LocalIp IF: " + localIp);
-            }
-            else
-            {
-                localIp = localIPs[0].ToString();
-                Debug.Log("LocalIp: Else" + localIp);
-            }
+    private string FindIp(IPAddress[] ips)
+    {
+        string ip;
 
-            Debug.Log("WHAT IS MY IP AFTER ALL THIS SHIT: " + localIp);
-            APIHelper.RegisterServer(localIp, PlayerPrefs.GetString("PlayerName"), "Arena01");
+        if (ips[1].ToString().Contains("25."))
+        {
+            ip = ips[1].ToString();
+        }
+        else
+        {
+            ip = ips[0].ToString();
         }
 
+        return ip;
     }
 }
