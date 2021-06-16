b95699421812157deea5044ca9d0cc44fb2e3d5e
Gracjan Redwanc <redwanc.gracjan@gmail.com>
2021-06-16 19:29:41 +0200

    Small visual updates and added player pause menu

---
+++ Assets/Scripts/UI/Player/PlayerPauseMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerPauseMenu : NetworkBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+    private bool IsPauseActive = false;
+
+    private void Update() 
+    {
+        if (!hasAuthority) { return; }
+
+        if (Input.GetButtonDown("Pause"))
+        {
+            IsPauseActive = !IsPauseActive;
+            pauseMenu.SetActive(IsPauseActive);
+        }
+    }
+
+    public void ClosePauseMenu()
+    {
+        IsPauseActive = !IsPauseActive;
+        pauseMenu.SetActive(IsPauseActive);
+    }
+
+    public void QuitMatch()
+    {
+        NetworkManager.singleton.ServerChangeScene("Menus");
+    }
+}
